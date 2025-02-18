7ca5dd24dfc5d86a272779a5aa3085d51a325986
Ekokotu Emmanuel Eguono <156138147+Elromena@users.noreply.github.com>
2025-02-18 14:33:48 +0100

    Update hotels.ts
    added Ibadan hotel

--- src/data/hotels.ts
+++ src/data/hotels.ts
@@ -1,6 +1,22 @@
 import { Hotel } from '../types';
 
 export const hotels: Hotel[] = [
+   // Ibadan Hotels
+  {
+    id: 'bon-nest',
+    name: 'Bon Hotel Nest',
+    description: 'Premier 4-star hotel in the heart of Ibadan.',
+    image: 'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa',
+    roomImages: [
+      'https://images.unsplash.com/photo-1595576508898-0ad5c879a061',
+      'https://images.unsplash.com/photo-1611892440504-42a792e24d32',
+      'https://images.unsplash.com/photo-1587985064135-0366536eab42'
+    ],
+    price: 75000,
+    destinationId: 'abuja',
+    features: ['Premium Dining', 'Wifi', 'Gym', 'Pool', 'Bar'],
+    rating: 4.7
+  },
   // Lagos Hotels
   {
     id: 'lagos-continental',
