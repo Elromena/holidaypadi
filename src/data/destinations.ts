510e26006e811259480e3cc827076eee773ef496
Ekokotu Emmanuel Eguono <156138147+Elromena@users.noreply.github.com>
2025-03-01 14:51:40 +0100

    Update destinations.ts
    Added abuja destination

--- src/data/destinations.ts
+++ src/data/destinations.ts
@@ -6,5 +6,11 @@ export const destinations: Destination[] = [
     name: 'Ibadan',
     description: 'Experience the cultural heart of Southwest Nigeria with a perfect mix of traditional landmarks and exciting modern attractions.',
     image: 'https://i0.wp.com/oyoaffairs.net/wp-content/uploads/2022/06/img_2752.jpg'
+  },
+  {
+    id: 'abuja',
+    name: 'Abuja',
+    description: 'Experience the cultural heart of Southwest Nigeria with a perfect mix of traditional landmarks and exciting modern attractions.',
+    image: 'https://i0.wp.com/oyoaffairs.net/wp-content/uploads/2022/06/img_2752.jpg'
   }
 ];
