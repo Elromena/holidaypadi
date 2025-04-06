2d2a60154c504fdfcd5dbec4921a15b9be609399
Ekokotu Emmanuel Eguono <156138147+Elromena@users.noreply.github.com>
2025-04-06 01:14:30 +0100

    Update destinations.ts

--- src/data/destinations.ts
+++ src/data/destinations.ts
@@ -5,12 +5,12 @@ export const destinations: Destination[] = [
     id: 'ibadan',
     name: 'Ibadan',
     description: 'Experience the cultural heart of Southwest Nigeria with a perfect mix of traditional landmarks and exciting modern attractions.',
-    image: 'https://i0.wp.com/oyoaffairs.net/wp-content/uploads/2022/06/img_2752.jpg'
+    image: 'https://flightpadi.com/wp-content/uploads/Ibadan.png'
   },
 {
   id: 'lagos',
   name: 'Lagos',
   description: 'As Nigeria\'s vibrant coastal metropolis, Lagos combines bustling markets and innovative tech hubs with stunning waterfront views, creating an energetic blend of tradition and ambition.',
-  image: 'https://flightpadi.com/wp-content/uploads/lagos-nigeria.jpg'
+  image: 'https://flightpadi.com/wp-content/uploads/lagos.png'
 }
 ];
